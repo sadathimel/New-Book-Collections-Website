591d8cba3cda877f4d3b5b39fba20a9d62861b7b
sadathimel <sadathossen.cse@gmail.com>
2024-09-19 12:52:53 +0600

    Add javascript for the header,the header element works fine

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/**
+ * add event on elements
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toogle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+/**
+ * active header when window scroll down 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    console.log(window.scrollY + "from if");
+  } else {
+    header.classList.remove("active");
+    console.log(window.scrollY + "from else");
+  }
+};
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
